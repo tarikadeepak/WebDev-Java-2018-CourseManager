33d68d660380797b01b4c4183257b8b42fc4faec
Deepak Tarika <tarikadeepak@gmail.com>
2019-03-09 13:15:09 -0500

    first commit

---
+++ src/main/java/com/example/demo/WebdevSummer2018Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdevSummer2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdevSummer2018Application.class, args);
+	}
+
+}
