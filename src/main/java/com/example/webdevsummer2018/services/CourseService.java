1c23c546b180de50d9a9b02ec1cb1022f32703d9
Deepak Tarika <tarikadeepak@gmail.com>
2019-03-10 16:44:56 -0400

    CourseService - allow cross origin access to service

--- src/main/java/com/example/webdevsummer2018/services/CourseService.java
+++ src/main/java/com/example/webdevsummer2018/services/CourseService.java
@@ -1,13 +1,17 @@
 package com.example.webdevsummer2018.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.webdevsummer2018.models.Course;
 import com.example.webdevsummer2018.repositories.CourseRepository;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge=3600)
 public class CourseService {
 	@Autowired
 	CourseRepository courseRepository;
@@ -15,5 +19,11 @@ public class CourseService {
 	public Iterable<Course> findAllCourses(){
 		return courseRepository.findAll();
 	}
+	
+	@PostMapping("/api/course")
+	public Course createCourse(@RequestBody Course course) {
+		return courseRepository.save(course);
+	}
+	
 	
 }
