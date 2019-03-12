f1d0c1749d9f69e9930e85e781ff012cc284cb84
Deepak Tarika <tarikadeepak@gmail.com>
2019-03-12 19:04:57 -0400

    Added Module, ModuleRepository and ModuleServices

---
+++ src/main/java/com/example/webdevsummer2018/repositories/ModuleRepository.java
@@ -0,0 +1,9 @@
+package com.example.webdevsummer2018.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.webdevsummer2018.models.Module;
+
+public interface ModuleRepository extends CrudRepository<Module,Integer>{
+
+}
