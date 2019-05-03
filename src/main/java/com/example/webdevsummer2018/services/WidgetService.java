0f61e9189b0f47b0b4cc3619419f4846d1da0ff0
Deepak Tarika <tarikadeepak@gmail.com>
2019-05-03 06:18:03 -0400

    Widget by ModuleId

--- src/main/java/com/example/webdevsummer2018/services/WidgetService.java
+++ src/main/java/com/example/webdevsummer2018/services/WidgetService.java
@@ -1,10 +1,12 @@
 package com.example.webdevsummer2018.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +23,12 @@ public class WidgetService {
 	@GetMapping("/api/widget")
 	public List<Widget> findAllWidgets(){
 		return (List<Widget>)widgetRepository.findAll();
+	}
+	
+	@GetMapping("/api/widget/{moduleId}")
+	public Optional<Widget> findWidgetsById(		
+			@PathVariable("moduleId") int moduleId){
+		return widgetRepository.findById(moduleId);
 	}
 	
 	@PostMapping("/api/widget/save")
